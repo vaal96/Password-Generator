b8da28332d576d8ec69d3fa8ae7b5d2487d37e1e
Valeria Aguilar <valeriaaguilar@live.com>
2021-11-02 09:09:07 -0700

    final update

--- Develop/script.js
+++ Develop/script.js
@@ -1,63 +1,75 @@
 // Assignment Code
 // var generateBtn = document.querySelector("btn");
-var symbols = ["!@#$%^&*_-+="];
-var letters = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"];
-var arrayLength = symbols.length
-var i;
-
-
-var passwordTxt = document.getElementById("password");
-var length = document.getElementById("length");
-var incNumbers = document.getElementById("numbers");
-var incSymbols = document.getElementById("symbols");
-var generateBtn = document.getElementById("generate");
+var symbolsA = "!@#$%^&*_-+=";
+var symbols = symbolsA.split(""); 
+var lettersA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var letters = lettersA.split(""); 
+var numbersA ="0123456789"
+var numbers = numbersA.split(""); 
+var lowerLettersA = "abcdefghijklmnopqrstuvwxyz";
+var lowerLetters = lowerLettersA.split("");
+//var resultP = "";
+//var charsType = [];
+//var i;
+//var length = passLength;
+//var passwordTxt = document.getElementById("password");
+//var length = document.getElementById("length");
+//var incNumbers = document.getElementById("numbers");
+//var incSymbols = document.getElementById("symbols");
+//ar generateBtn = document.getElementById("generate");
 //const generateBtn = document.getElementById("generate");
+//dynamic vars
+window.onload = alert("Click 'Generate Password' to start!");
 
 
- 
 
 function generatePassword() {
+  var resultP = "";
+  var charsType = [];
   // TODO: Your code to generate a password goes here. Be sure to return a password
-  
-  // string. You will need to change the following two lines of code.
-   letters = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"];
-   symbols = ["!@#$%^&*_-+="];
-   //password = [""];
-
+  var lengthT = prompt("How long would you like your password to be?");
    
+  if(lengthT < 8 || lengthT > 128){
+    alert("Password must be between 8 to 128 characters long.\nTry again")
+    return generatePassword();
+  }
 
-}
+  if (confirm("Include lowercase letters?")){
+    charsType = charsType.concat(lowerLetters)
+  }
+  if (confirm("Include uppercase letters?")){
+    charsType = charsType.concat(letters)
+  }
+  if (confirm("Include numbers?")){
+    charsType = charsType.concat(numbers)
+  }
+  if (confirm("Include symbols?")){
+    charsType = charsType.concat(symbols)
+  } for (var i = 0; i < lengthT; ++i){
+        var random = Math.floor(Math.random() * charsType.length);
+        resultP += charsType[random];
+      }
+      document.getElementById("password").innerHTML = resultP;
+
+
+  
+};
  // var password = "password1234";
   // return password;
 
+  //generateBtn.addEventListener("click", writePassword);
   
-  generateBtn.addEventListener("click", writePassword);
-  // document.getElementById("generate").innerHTML = password;
+  
 
-
-// Write password to the #password input
- function writePassword() {
-  for (i = 0; i < 10; i++) {
-    let generatedPassword = letters[Math.floor(Math.random() * 62)]
-  }
-
-  var password = generatePassword();
-  generatePassword(); 
+// this snip?
+generatePassword(); 
+document.getElementById("password").innerText = resultP;
   
  // passwordTxt = document.querySelector("#password");
 
-  passwordTxt.value = password;
+
+ // bottom line 
+//resultP.value = password;
 //  passwordTxt.textContent = password;
 
-}
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-
-  // let characters = letters;
-  // incNumbers(characters += numbers) = "";
-  // incSymbols (characters += symbols) = "";
-  // incNumbers.checked ? (characters += numbers) : "";
-  // incSymbols.checked ? (characters += symbols) : "";
-  // passwordTxt.value = generatePassword(length.value, characters);
